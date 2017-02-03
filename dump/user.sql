aea28b7b8d4f04860ba3c182e624d1ee8c85fcca
rendynata025 <rendynata025@gmail.com>
2017-02-03 14:51:21 +0700

    add dump user

---
+++ dump/user.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03 Feb 2017 pada 08.50
+-- Versi Server: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `magang-gnfi`
+--
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `username`, `email`, `password`, `created`) VALUES
+(1, 'Rendy', 'rendy', 'email@rendy.com', '$2y$10$d1MqzN9AkvnQS4uCLy9jvuqzaHwT2zzjbp9OHgampNI3HUg8K5A5i', '2017-02-03 08:45:04');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
