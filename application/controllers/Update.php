cd5684b9d9aa0e3d00a3db3ef1e4b47322b0a2b0
el-fath <rochman003@gmail.com>
2017-02-03 15:51:11 +0700

    update_fix a

--- application/controllers/Update.php
+++ application/controllers/Update.php
@@ -3,26 +3,69 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Update extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('user_model');
+	}
+
 	public function index()
 	{
-		$this->load->view('update');
-		$this->load->model('User_model');
-		$this->load->select();
-		$data['user'] = $this->User_model->select();
+		redirect('','refresh');
 	}
+
+	public function edit($id)
+	{
+		if(empty($id)) {
+			die('id belum diisi');
+		}
+
+		// $this->load->model('user_model');
+		$user = $this->user_model->select($id);
+
+		if(empty($user)) 
+		{
+			die('user tidak ditemukan');
+		}
+
+		$data['user'] = $user;
+		$this->load->view('update',$data);
+	}
+
+	public function save()
+	{
+		$this->form_validation->set_rules('user_id', 'user_id', 'required|trim|is_natural');
+		$this->form_validation->set_rules('name', 'Name', 'required|trim');
+		$this->form_validation->set_rules('username', 'username', 'required|trim|min_length[3]|max_length[10]');
+		$this->form_validation->set_rules('email', 'email', 'required|trim|valid_email');
+		if($this->form_validation->run() === true) 
+		{
+			$user_id = $this->input->post('user_id');
+			$name = $this->input->post('name');
+			$username = $this->input->post('username');
+			$email = $this->input->post('email');
+			$data=array(
+				'name' => $name,
+				'username' => $username,
+				'email' => $email
+			);
+			// $this->load->model('user_model');
+			$cek = $this->user_model->update($user_id,$data);
+			if($cek) 
+			{
+				echo "berhasil di edit : " . $cek;
+			}
+			else 
+			{
+				echo "gagal bro";
+			}
+		}
+		else
+		{
+			echo validation_errors();
+		}
+		
+		// redirect('');
+	}
+
 }
--- application/views/update.php
+++ application/views/update.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>form Edit</title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-	<h1 align="center">Form Edit User</h1>
-	<table align="center" border="2">
-		<caption>Tabel Edit</caption>
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Name</th>
-				<th>Username</th>
-				<th>Email</th>
-				<th>Password</th>
-			</tr>
-		</thead>
-		<?php $no=0; foreach ($table as $u) { $no++; ?>
-		<tbody>
-			<tr>
-				<td><?php echo $no; ?></td>
-				<td><?php echo $p->id; ?></td>
-				<td></td>
-			</tr>
-		</tbody>
-		<?php } ?>
-	</table>
-</body>
+  <head>
+	<title>Halaman Create</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('asset/css/bootstrap.min.css') ?>">
+  </head>
+  <body>
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4">
+      	<form action="<?php echo site_url('update/save') ?>" method="post">
+      	  <div class="form-group">
+            <label>Nama</label>
+            <input type="text" name="name" class="form-control" value="<?=$user->name?>" placeholder="Masukkan nama">
+            <input type="hidden" name="user_id" value="<?=$user->id?>">
+          </div>
+
+      	  <div class="form-group">
+            <label>Username</label>
+            <input type="text" name="username" class="form-control" value="<?=$user->username?>"placeholder="Masukkan Username">
+          </div>
+
+      	  <div class="form-group">
+            <label>Email</label>
+            <input type="email" name="email" class="form-control" value="<?=$user->email?>" placeholder="Masukkan Email">
+          </div>
+          <input type="submit" class="btn btn-warning" value="Simpan">
+      	</form>
+      </div>	
+    </div>
+
+    <script src="<?php echo base_url('') ?>./asset/js/jquery-1.11.1.min.js"></script>
+    <script src="<?php echo base_url('') ?>./asset/js//bootstrap.min.js"></script>
+  </body>
 </html>
